72602a1143dbe2e3e16ed592ce68e219c01c5d0a
rslangl <fjellape1447@protonmail.com>
2025-03-12 19:54:26 +0100

    fix: xdg path

--- src/config.rs
+++ src/config.rs
@@ -14,12 +14,12 @@ pub fn get_cfg() -> Result<Config, String> {
 
     if let Ok(base_dirs) = base_dirs {
 
-        match base_dirs.create_data_directory("config") {
+        match base_dirs.create_data_directory("data") {
             Ok(data_dir) => println!("Data directory created at: {:?}", data_dir),
             Err(e) => println!("Failed to create data directory: {}", e),
         }
 
-        match base_dirs.create_config_directory("data") {
+        match base_dirs.create_config_directory("config") {
             Ok(cfg_dir) => println!("Config directory created at: {:?}", cfg_dir),
             Err(e) => println!("Failed to create config directory: {}", e),
         }
