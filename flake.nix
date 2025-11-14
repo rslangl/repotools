a043275ffff75cc40abafdb4d17dd4205633e7fc
rslangl <fjellape1447@protonmail.com>
2025-11-14 21:23:26 +0100

    feat: add nix shell

---
+++ flake.nix
@@ -0,0 +1,29 @@
+{
+  description = "Rust dev environment for NixOS projects";
+
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+
+  outputs = { self, nixpkgs }: 
+    let 
+      system = "x86_64-linux";
+      pkgs = import nixpkgs { inherit system; };
+    in {
+      devShells.default = pkgs.mkShell {
+        buildInputs = [
+          pkgs.rustup
+          pkgs.rustc
+          pkgs.cargo
+          pkgs.clang
+          pkgs.pkgconfig
+          pkgs.openssl
+          pkgs.zlib
+        ];
+
+        shellHook = ''
+          export CARGO_TARGET_X86_64_UNKNOWN_LINUX_GNU_LINKER=${pkgs.clang}/bin/clang
+          export RUSTFLAGS="-C linker=${pkgs.clang}/bin/clang"
+          echo "Rust dev shell ready!"
+        '';
+      };
+    };
+}
