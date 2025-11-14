5f49da3114aa5e0f454d41e9129b42cadbc378f7
rslangl <fjellape1447@protonmail.com>
2025-11-14 21:29:34 +0100

    fix: use system with devShell

--- flake.nix
+++ flake.nix
@@ -4,26 +4,30 @@
   inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
 
   outputs = { self, nixpkgs }: 
-    let 
+    let
       system = "x86_64-linux";
       pkgs = import nixpkgs { inherit system; };
     in {
-      devShells.default = pkgs.mkShell {
-        buildInputs = [
-          pkgs.rustup
-          pkgs.rustc
-          pkgs.cargo
-          pkgs.clang
-          pkgs.pkgconfig
-          pkgs.openssl
-          pkgs.zlib
-        ];
+      devShells = {
+        x86_64-linux = {
+          default = pkgs.mkShell {
+            buildInputs = [
+             pkgs.rustup
+             pkgs.rustc
+             pkgs.cargo
+             pkgs.clang
+             pkgs.pkg-config
+             pkgs.openssl
+             pkgs.zlib
+          ];
 
-        shellHook = ''
-          export CARGO_TARGET_X86_64_UNKNOWN_LINUX_GNU_LINKER=${pkgs.clang}/bin/clang
-          export RUSTFLAGS="-C linker=${pkgs.clang}/bin/clang"
-          echo "Rust dev shell ready!"
-        '';
+          shellHook = ''
+            export CARGO_TARGET_X86_64_UNKNOWN_LINUX_GNU_LINKER=${pkgs.clang}/bin/clang
+            export RUSTFLAGS="-C linker=${pkgs.clang}/bin/clang"
+            echo "Rust dev shell ready!"
+          '';
+        };
       };
     };
+  };
 }
