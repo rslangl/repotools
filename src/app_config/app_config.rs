0f06bfd73f0d2718bcadbb3bd655a21efe9f6a7b
rslangl <fjellape1447@protonmail.com>
2025-11-15 20:11:43 +0100

    fix: actually use passed config path

--- src/app_config/app_config.rs
+++ src/app_config/app_config.rs
@@ -1,11 +1,9 @@
 //! config/config.rs
 
-static REPOTOOLS_DEFAULT_CONFIG_PATH: &str = "~/.config/repotools/config";
-
 pub fn get_config(file_path: std::path::PathBuf) -> Result<std::collections::HashMap<String, String>, Box<dyn std::error::Error>> {
 
     let config = config::Config::builder()
-        .add_source(config::File::with_name("~./config/repotools/config"))
+        .add_source(config::File::with_name(file_path.to_str().expect("Invalid config file path")))
         .build()?
         .try_deserialize::<std::collections::HashMap<String, String>>()?;
 
