ad8370c458bf67809ae43c8829a7ea988b47a420
rslangl <fjellape1447@protonmail.com>
2025-12-17 20:49:10 +0100

    chore: improve error handling for app config creation

--- src/main.rs
+++ src/main.rs
@@ -4,16 +4,31 @@ use clap::{Parser};
 use crate::cli::{Cli, Command};
 
 use repotools::cli;
-use repotools::app_config::app_config;
+use repotools::app_config::app_config::{self, ConfigError};
 use repotools::initializers::init_project;
 
-fn main() {
+// TODO: add more as needed
+#[derive(Debug)]
+enum AppError {
+    Config(ConfigError)
+}
+
+impl From<ConfigError> for AppError {
+    fn from(e: ConfigError) -> Self {
+        AppError::Config(e)
+    }
+}
+
+fn main() -> Result<(), AppError> {
 
     let cli = Cli::parse();
 
-    let config = app_config::get_config(cli.global.config_path).expect("Could not load app config");
+    let config = app_config::get_config(cli.global.config_path)?; //.expect("Could not load app config");
 
     match cli.command {
         Command::InitProject(args) => init_project::handle(args, config).expect("Could not initialize project") // for global args, add &cli.global
+        // TODO: Command::AddFeature(args)
     }
+
+    Ok(())
 }
