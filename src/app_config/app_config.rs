04376bed70d8cd37006effea89be74d49ac8257a
rslangl <fjellape1447@protonmail.com>
2026-05-01 14:54:06 +0200

    chore: pretty print output

--- src/app_config/app_config.rs
+++ src/app_config/app_config.rs
@@ -97,6 +97,8 @@ pub struct ProjectTemplate {
     pub template_files: PathBuf,
 }
 
+// TODO: ProjectTemplate ToString() for printout in extras
+
 pub fn get_config(file_path: Option<String>) -> Result<AppConfig, ConfigError> {
     let config_path = match file_path {
         Some(path) => PathBuf::from(path),
--- src/extras/extras.rs
+++ src/extras/extras.rs
@@ -1,6 +1,6 @@
 use clap::{Args, Parser, ValueEnum};
 
-use std::fmt;
+use std::{fmt, fmt::Write};
 
 use crate::{
     app_config::AppConfig,
@@ -38,13 +38,41 @@ pub struct ListItemArgs {
 
 pub fn list_items(args: ListItemArgs, config: AppConfig) -> Result<(), String> {
 
-    // Prints all available projects that is configured
-    config
+    let mut out = String::new();
+
+    // Gets all available projects that is configured
+    write!(out, "Project templates:\n------------------\n");
+
+    let templates = config
         .templates
         .iter()
         .for_each(|t|
-            println!("{}", t.name)
+            write!(out, "Type: {}\nProfile: {}\n\n", t.name, t.profile).unwrap()
         );
+    
+    // Gets all available licenses that is configured
+    write!(out, "Feature: Licenses:\n------------------\n");
+
+    let features = config
+        .features
+        .licenses
+        .iter()
+        .for_each(|t|
+            write!(out, "Name: {}\n\n", t.name).unwrap()
+        );
+
+    // get all available linters that is configured
+    write!(out, "Feature: Linters:\n-----------------\n");
+
+    let linters = config
+        .features
+        .linters
+        .iter()
+        .for_each(|t|
+            write!(out, "Type: {}\n\n", t.name).unwrap()
+        );
+
+    println!("{}", out);
 
     Ok(())
 }
